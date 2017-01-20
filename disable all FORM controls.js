c5decf1eaef7ed7558d90d70464d0a757e1d1224
DESKTOP-84728BB\Marvin <marvinnagaguerrero@gmail.com>
2017-01-20 17:22:59 +0800

    initial

---
+++ disable all FORM controls.js
@@ -0,0 +1,13 @@
+function makeControlsReadOnly() 
+{
+    console.log('makeControlsReadOnly');
+    var allControls = Xrm.Page.getControl();
+    var controlsLenght = allControls.length;
+     for (var i = 0; i < controlsLenght; i++)
+     {
+            allControls[i].setDisabled(true);
+         //   console.log(allControls[i]);
+     }
+}
+
+
